008b8685cb86298bc6c4afa2215ef4bcab1f5802
Lesya-Brodskaya <110602282+Lesya-Brodskaya@users.noreply.github.com>
2023-02-17 16:27:11 +0200

    02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const videoplayerCurrentTime = function(time) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(time.seconds));
+    const timeKey = Number(localStorage.getItem("videoplayer-current-time"));
+    console.log(timeKey);
+}
+  
+player.on('timeupdate', videoplayerCurrentTime);
+
+player.setCurrentTime(30.456).then(function(seconds) {}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
+
+// player.getVideoTitle().then(function(title) {
+//     console.log('title:', title);
+// });
