d4da24472784a8e16218f10727d6b45f09c4d39f
Lesya-Brodskaya <110602282+Lesya-Brodskaya@users.noreply.github.com>
2023-02-23 12:41:59 +0200

    update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,40 +2,38 @@ import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
 const formData = {};
-
 const form = document.querySelector('.feedback-form');
-
-const savedKey = localStorage.getItem(STORAGE_KEY);
 
 populateTextarea();
 
 form.addEventListener('submit', onFormSubmit);
-form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('input', throttle(onFormInput, 500));
 
 function onFormSubmit(evt) {
   evt.preventDefault();
-
-  console.log(formData);
-  evt.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
+  evt.currentTarget.reset();
 }
 
-function onInput(evt) {
+function onFormInput(evt) {
   formData[evt.target.name] = evt.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  console.log(formData);
 }
 
 function populateTextarea() {
-  try {
-    if (savedKey) {
-      const parseData = JSON.parse(savedKey);
-      const formKeys = Object.entries(parseData);
-      formKeys.forEach(([name, value ]) => {
-        form[name];
+  if (formData) {
+    let formData = localStorage.getItem(STORAGE_KEY);
+    try {
+      formData = JSON.parse(formData);
+      Object.entries(formData).forEach(([name, value]) => {
+        form.elements[name].value = value;
       });
+    } catch (error) {
+      console.error(
+        'Error: invalid saved form state in LocalStorage!' + STORAGE_KEY
+      );
+      console.error(error);
     }
-  } catch (error) {
-      console.log(error.name);
-    
   }
-};
+}
